355e4025adb9a3af4d3f39859e9c110b782b76da
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2025-02-10 09:52:25 -0600

    Mostrar listadode favoritos

---
+++ src/components/pokemons/FavoritePokemons.tsx
@@ -0,0 +1,24 @@
+
+import { For, createSignal } from 'solid-js';
+import type { FavoritePokemon } from '../../interfaces/favorite-pokemon';
+
+const getLocalStoragePokemons = () => {
+    const favoritePokemons = JSON.parse(
+        localStorage.getItem('favorites') ?? '[]'
+    )
+
+    return favoritePokemons
+} 
+
+export const FavoritePokemons = () => {
+    const [pokemons , setPokemons] = createSignal(getLocalStoragePokemons());
+    return (
+        <div class="grid grid-cols-2 sm:grid-cols-4">
+            <For each={pokemons()}>
+                {
+                    (pokemon) => <h1>{pokemon.name}</h1>
+                }
+            </For>
+        </div>
+    )
+}
